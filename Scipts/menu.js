4933b8c9904c77134363263e454bc199539c4eb9
HJeromePerera <jerome-perera@outlook.com>
2025-01-19 12:14:09 +0530

    Menu patch.

--- Scipts/menu.js
+++ Scipts/menu.js
@@ -4,12 +4,14 @@ const menuWindow = document.querySelector(".menu");
 
 open.addEventListener("click", (el) => {
   menuWindow.classList.add("menu-open-anim");
-  menuWindow.classList.remove("hidden");
+  // menuWindow.classList.remove("hidden");
+  menuWindow.style.display = "flex";
 });
 
 close.addEventListener("click", (el) => {
   menuWindow.classList.remove("menu-open-anim");
   // close.classList.add("close-menu-btn-anim");
+  menuWindow.style.display = "none";
 
-  menuWindow.classList.add("hidden");
+  // menuWindow.classList.add("hidden");
 });
